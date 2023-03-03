6caba91e9632b43fb62f33239954538649676663
raminalaie.79@gmail.com <109432088+raminalaie@users.noreply.github.com>
2023-03-04 00:21:56 +0330

    startproject and startapp account

---
+++ accounts/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ("email", "is_superuser", "is_active")
+    list_filter = ("email", "is_superuser", "is_active")
+    search_fields = ("email",)
+    ordering = ("email", )
+    fieldsets = (
+        ("Authentication", {
+            "fields": ("email", "password")
+        }),
+        ("Permissions", {
+            "fields": ("is_staff", "is_active", "is_superuser", "groups", "user_permissions")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1", "password2", "is_staff", "is_superuser",
+                "is_active", "groups", "user_permissions",
+            )}
+         ),
+    )
+admin.site.register(User, CustomUserAdmin)
