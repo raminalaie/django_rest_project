ae9f324d37296267a7f8dd7898ea89200f70ea8e
raminalaie.79@gmail.com <109432088+raminalaie@users.noreply.github.com>
2023-04-24 19:22:01 +0330

    add filter coustom

--- blog/api/v1/views.py
+++ blog/api/v1/views.py
@@ -13,6 +13,8 @@ from .permissions import IsOwnerOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from .paginations import DefaultPagination
+from .filters import PostFilters
+
 # @api_view(["GET","POST"])
 # @permission_classes([IsAuthenticated])
 # def api_post_list_view(request):
@@ -114,7 +116,8 @@ class PostModelViewSet(viewsets.ModelViewSet):
         serializer_class = PostSerializers
         queryset = Post.objects.filter(status=True)
         filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-        filterset_fields = ['category', 'author', "status"]
+        # filterset_fields = ['category', 'author', "status"]
+        filterset_class = PostFilters
         search_fields = ['title', 'content']
         ordering_fields = ["published_date"]
         pagination_class = DefaultPagination
